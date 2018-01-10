94a45970cb376a66e908ef5f167134e852256b92
maureenv <maureenvogel@outlook.com>
2018-01-09 21:57:44 -0500

    Add initial website files

---
+++ app/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  /// fade in C overlay
+  $('.overlayc').fadeIn(2000);
+
+  ///// Small navbar drop down animation
+  $(".nav-icon").click(function(){
+    $(".slide-down-nav").toggleClass("move-nav-down");
+    $(".small-logo").toggleClass("no-display");
+    });
+
+  $(".slide-down-li").click(function(){
+    $(".slide-down-nav").toggleClass("move-nav-down");
+    $(".small-logo").toggleClass("no-display");
+  });
+
+
+  //// navbar scroll to link effect
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+
+  var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+
+});
