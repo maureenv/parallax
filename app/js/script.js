183bf40570c4a28251be3d585c5ebc807f8b3379
maureenv <maureenvogel@outlook.com>
2018-01-11 23:02:24 -0500

    Add animations to header text

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,35 @@
 $(document).ready(function(){
-  /// fade in C overlay
-  $('.overlayc').fadeIn(2000);
+  //$('.overlayc').fadeIn(2000);
 
-  ///// Small navbar drop down animation
-  $(".nav-icon").click(function(){
-    $(".slide-down-nav").toggleClass("move-nav-down");
-    $(".small-logo").toggleClass("no-display");
-    });
+  // Typewriter animation
+  var i = 0;
+  var text = 'Hello, my name is Maureen and I am a';
 
-  $(".slide-down-li").click(function(){
-    $(".slide-down-nav").toggleClass("move-nav-down");
-    $(".small-logo").toggleClass("no-display");
-  });
+  function typeWriter() {
+    if ( i < text.length ) {
+      document.querySelector('.intro h2').innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 60);
+    }
+  }
+
+  typeWriter();
+  setTimeout(slideRight, 1700);
+
+  // Typewriter animation
+  var wordIndex = 0;
+
+  function slideRight() {
+    var words = $('.intro h1');
+    if ( wordIndex < words.length ) {
+      $('.intro h1:eq(' + wordIndex + ')').addClass('zero-margin-left');
+      wordIndex ++;
+      setTimeout(slideRight, 1000);
+    }
+  }
+
+
+
 
 
   //// navbar scroll to link effect
